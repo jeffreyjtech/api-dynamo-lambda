6b3f253b9007fad64f076f0cddd253c3a4fba879
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-14 08:56:30 -0700

    Updated workflow and code to use peopleRead

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 // 3rd party libraries
 const dynamoose = require('dynamoose');
 
-// Create our schema 
-// These are not validated since DynamoDB is schema-less
-const friendSchema = new dynamoose.Schema({
+const peopleSchema = new dynamoose.Schema({
   id: String,
   name: String,
-  phone: String,
+  age: Number,
 });
 
 // The first argument MUST match the table name in DynamoDB
 // The 2nd argument MUST be a schema
-const friendModel = dynamoose.model('friends', friendSchema)
+const peopleModel = dynamoose.model('people', peopleSchema)
 
 exports.handler = async (event) => {
   // const { pathParameters, queryStringParameters } = event;
@@ -20,19 +18,19 @@ exports.handler = async (event) => {
 
   // NEVER declare response with const
   let response = { statusCode: null, body: null };
-  let friendRecords = [];
+  let peopleRecords = [];
   
   try {
     // perform the CRUD using our specified schema
     if(id){
       // This queries using the id
-      friendRecords = await friendModel.query('id').eq(id).exec();
+      peopleRecords = await peopleModel.query('id').eq(id).exec();
     } else {
       // This scans all
-      friendRecords = await friendModel.scan().exec();
+      peopleRecords = await peopleModel.scan().exec();
     }
     response.statusCode = 200;
-    response.body = JSON.stringify(friendRecords);
+    response.body = JSON.stringify(peopleRecords);
   } catch (e) {
     console.error(e);
     response.statusCode = 500;
