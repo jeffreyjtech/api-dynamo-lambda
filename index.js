9b56030db6fb4a5956046ec7365b2f942578de9f
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-14 14:51:51 -0700

    Created switch case for different HTTP methods

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // 3rd party libraries
 const dynamoose = require('dynamoose');
 
+const {
+  getHandler,
+  postHandler,
+  putHandler,
+  deleteHandler,
+} = require('./routes');
+
 const peopleSchema = new dynamoose.Schema({
   id: String,
   name: String,
@@ -9,32 +16,34 @@ const peopleSchema = new dynamoose.Schema({
 
 // The first argument MUST match the table name in DynamoDB
 // The 2nd argument MUST be a schema
-const peopleModel = dynamoose.model('people', peopleSchema)
+const peopleModel = dynamoose.model('people', peopleSchema);
 
 exports.handler = async (event) => {
-  // const { pathParameters, queryStringParameters } = event;
-  const { pathParameters } = event;
+  const { pathParameters, jsonBody, httpMethod  } = event;
   const id = pathParameters.id || null;
+  const body = JSON.parse(jsonBody);
 
-  // NEVER declare response with const
   let response = { statusCode: null, body: null };
-  let peopleRecords = [];
-  
-  try {
-    // perform the CRUD using our specified schema
-    if(id){
-      // This queries using the id
-      peopleRecords = await peopleModel.query('id').eq(id).exec();
-    } else {
-      // This scans all
-      peopleRecords = await peopleModel.scan().exec();
-    }
-    response.statusCode = 200;
-    response.body = JSON.stringify(peopleRecords);
-  } catch (e) {
-    console.error(e);
-    response.statusCode = 500;
-    response.body = JSON.stringify(e);
+
+  switch (httpMethod) {
+    case 'GET':
+      response = await getHandler(id, response, peopleModel);
+      break;
+      
+    case 'POST':
+      response = await postHandler(body, response, peopleModel);
+      break;
+
+    case 'PUT':
+      response = await putHandler(body, response, peopleModel);
+      break;
+
+    case 'DELETE':
+      response = await deleteHandler(id, response, peopleModel);
+      break;
+    
+    default:
+      response = { statusCode: 404, body: 'Invalid method' };
   }
 
   return response;
